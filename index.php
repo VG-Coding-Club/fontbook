8264017e27ff21dbeceed30561893edd03451ab8
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-10-12 21:20:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title> Font Book | How to Coding </title>
+<title> Font Book </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/coding/js/randomcolor.js"></script>
 <script type="text/javascript">
@@ -64,7 +64,7 @@ margin:5% 0 10%;
 </style>
 </head>
 <body>
-<h1 id="title">Font Book | VG CTLG</h1>
+<h1 id="title">Font Book</h1>
 <div id="cover">
 <div id="left">
 <div id="hello"></div>
