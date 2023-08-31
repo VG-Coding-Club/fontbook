f4c516b6b1cc3e37a8b09d8a494ff0eefbd5ccaf
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-08-31 23:08:54 +0900

    fonts

--- index.js
+++ index.js
@@ -105,44 +105,15 @@ function indexItems(obj) {
     for (const thing of thingAll) {
         const thingLi = document.createElement('li');
         thingLi.setAttribute("data-org", thing.org);
+        thingLi.setAttribute("data-name", thing.name);
         thingLi.classList.add(thing.class);
         thingLi.innerHTML = `
-        <h3>
-        ${thing.name}<br/>
-        <small>by ${thing.by}</small>
-        </h3>
-        <p hidden>${thing.description}</p>
+        <h3>${thing.name}</h3>
+        <small>by <i>${thing.by}</i></small>
+        <p>${thing.description}</p>
         `
         thingsUL.appendChild(thingLi);
     }
 }
 
 indexJSON()
-
-window.onload = function () {
-    let targets = document.querySelectorAll("#things ul li")
-    let filter = document.querySelectorAll('#org input[type="radio"]')
-    if (filter) {
-        //****** for all select ******
-        for (let i of filter) {
-            i.addEventListener('change', () => {
-                let value = i.value
-                let name = i.getAttribute('name')
-                //*** for each target ***
-                for (let ii of targets) {
-                    //*** delete hidden class ***
-                    ii.classList.remove('hidden')
-                    ii.hidden = false
-                    //*** check target every select ***
-                    let item_data = ii.getAttribute('data-' + name)
-                    //*** set hidden class ***
-                    if (value && value !== 'all' && value !== item_data && !ii.classList.contains('hidden')) {
-                        ii.classList.add('hidden')
-                        ii.hidden = true
-                    }
-                }
-            })
-        }
-    }
-};
-
